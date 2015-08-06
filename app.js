30d6c7ec1327eadef12bb2db28f5a3381cfdfcd0
Lukasz Kidzinski <kidzik@Lukaszs-Mac-mini.local>
2015-08-06 23:33:47 +0200

    Results page, lolo requests moved to app.js, added frame for the results

--- app.js
+++ app.js
@@ -29,8 +29,47 @@ app.get('/', function(req, res) {
 });
 // [END hello_world]
 
-app.get('/london.html', function(req, res) {
-  res.sendfile('london.html');
+app.get('/results/', function(req, res) {    
+  res.sendfile('results.html');
+});
+
+function getLon()
+{
+    var r = new Object();
+    r.User = "lor.lucignano@gmail.com";
+    r.Pass = "675dd12f8800bc68f38726a4c541ecdeb87beed1";
+    r.Environment = "fast_search_1_0";
+    r.Origin = "LON";
+    r.DepartureFrom = "2016-01-13";
+    r.LengthOfStay = [4,5,6];
+    var date = new Date()
+    r.DepartureFrom = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDay();
+    r.DepartureTo = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDay()+4);
+    console.log(r.DepartureFrom)
+    console.log(r.DepartureTo)
+    r.PriceMax = 1000;
+    var url = 'https://fs-json.demo.vayant.com:7081';
+    var rbody = JSON.stringify(r);
+    var invocation = new XMLHttpRequest();
+    invocation.open('POST', url, true);
+    invocation.setRequestHeader('Content-Type', 'application/json');
+    invocation.onreadystatechange = function(data){
+	if(invocation.readyState == 4 && invocation.status == 200)
+	{
+	    $( "#result" ).text(invocation.responseText)
+	    console.log(invocation.responseText);
+	}
+    };
+    invocation.send(rbody);
+}
+
+app.post('/request/', function(req, res) {
+    // TODO: 
+    // 1) Read origins from req
+    // 2) Run requests from origins
+    // 3) Save each result in appropriate place in dictionary of airports
+    // 4) Go through all airports and get the cheapest connections
+    // 5) Return connections
 });
 
 // [START server]
